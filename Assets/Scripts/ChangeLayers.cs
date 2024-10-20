01982372beb60e362680d6b4072fd49ed52b85ac
Arturchik2515 <shumihinartur@gmail.com>
2024-10-20 20:32:40 +0700

    20.10.2024

--- Assets/Scripts/ChangeLayers.cs
+++ Assets/Scripts/ChangeLayers.cs
@@ -9,15 +9,18 @@ public class ChangeLayers : MonoBehaviour
     public GameObject EnterMenu;
     public GameObject ConditionMenu;
     public GameObject UserEnterMenu;
+    public GameObject Categories;
+    public GameObject SwimmingPool;
     // Start is called before the first frame update
     public void Start()
     {
         EnterMenu.SetActive(true);
-        UserEnterMenu.SetActive(false);
-        MainMenu.SetActive(false);
         RegMenu.SetActive(false);
+        MainMenu.SetActive(false);
         ConditionMenu.SetActive(false);
-            
+        UserEnterMenu.SetActive(false);
+        Categories.SetActive(false);
+        SwimmingPool.SetActive(false);
     }
 
     public void ChangeLayer(GameObject nextLayer)
@@ -25,6 +28,13 @@ public class ChangeLayers : MonoBehaviour
         EnterMenu.SetActive(false);
         nextLayer.SetActive(true);
         EnterMenu = nextLayer;
+
+    }
+
+    public void PreviousLayer(GameObject preLayer)
+    {
+        
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,16 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SceneChange : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public void ChangeScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
-    
-
     // Start is called before the first frame update
     void Start()
     {
