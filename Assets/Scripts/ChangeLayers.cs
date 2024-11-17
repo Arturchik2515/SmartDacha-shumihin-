7a7ce1e82ec5dab41baca1e2fd92472201523077
Arturchik2515 <151436717+Arturchik2515@users.noreply.github.com>
2024-11-17 20:27:30 +0700

    17.11.2024(Смена окон)

--- Assets/Scripts/ChangeLayers.cs
+++ Assets/Scripts/ChangeLayers.cs
@@ -1,40 +1,38 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeLayers : MonoBehaviour
 {
-    public GameObject MainMenu;
-    public GameObject RegMenu;
-    public GameObject EnterMenu;
-    public GameObject ConditionMenu;
-    public GameObject UserEnterMenu;
-    public GameObject Categories;
-    public GameObject SwimmingPool;
+    public GameObject[] Layers;
+    int prevLayer;
+    public int currentIndex;
+
     // Start is called before the first frame update
     public void Start()
     {
-        EnterMenu.SetActive(true);
-        RegMenu.SetActive(false);
-        MainMenu.SetActive(false);
-        ConditionMenu.SetActive(false);
-        UserEnterMenu.SetActive(false);
-        Categories.SetActive(false);
-        SwimmingPool.SetActive(false);
+       
     }
 
-    public void ChangeLayer(GameObject nextLayer)
+    public void OffLayers()
     {
-        EnterMenu.SetActive(false);
-        nextLayer.SetActive(true);
-        EnterMenu = nextLayer;
-
+        foreach (GameObject allLayers in Layers)
+        {
+            allLayers.SetActive(false);
+        }
+    }
+    public void ChangeLayer(int index)
+    {
+        OffLayers();
+        Layers[index].SetActive(true);
+        prevLayer = currentIndex;
+        currentIndex = index;
     }
 
-    public void PreviousLayer(GameObject preLayer)
+    public void PreviousLayer()
     {
-        
-
+       ChangeLayer(prevLayer);
     }
 
     // Update is called once per frame
